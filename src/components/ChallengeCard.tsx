633d5c9870c837ed716254d80c4fe635bb99b338
kirill.d <denisov.k.o@yandex.ru>
2026-01-29 22:06:40 +0300

    hot fix

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -269,7 +269,7 @@ export function ChallengeCard({challenge, onShare, onEdit, onAccept, onLeave, on
             </div>
           }
           {
-            onOpenReports && isCreator &&
+            onOpenReports && isCreator && challenge.requiresReport &&
             <div className="flex justify-center py-2">
               <Button onClick={(e) => (e.stopPropagation(), onOpenReports(challenge.id))}>
                 Отчёты
