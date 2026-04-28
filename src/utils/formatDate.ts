abdf8f71c79bdd1af0997bb8130e9a810bfda349
kirill.d <denisov.k.o@yandex.ru>
2026-04-28 19:50:22 +0300

    0.5.10

--- src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: Date): string => {
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const year = date.getUTCFullYear();
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-  return `${hours}:${minutes}:${seconds} UTC ${day}.${month}.${year}`;
-};
